34fee36bf582a5511863ba498408ecf0d7c05a2b
Greg Miller <hello@gregmiller.io>
2025-08-27 12:39:33 -0400

    chore: update configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-paper.pages.dev/",
+  site: "https://www.devcharms.com/",
   integrations: [
     tailwind({
       config: {
