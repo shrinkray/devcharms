057839e8382aaadd91ed9023ec78e2e8bcbaf12c
Greg Miller <hello@gregmiller.io>
2025-08-27 09:36:17 -0400

    chore(cursor): add new cursorrules guidelines

---
+++ tina/config.ts
@@ -0,0 +1,47 @@
+import { defineConfig } from "tinacms";
+import { blog_postFields } from "./templates";
+
+const config = defineConfig({
+  clientId: process.env.ASTRO_PUBLIC_TINA_CLIENT_ID!,
+  branch:
+      process.env.ASTRO_PUBLIC_TINA_BRANCH! || // custom branch env override
+      process.env.ASTRO_PUBLIC_VERCEL_GIT_COMMIT_REF! || // Vercel branch env
+      process.env.HEAD!, // Netlify branch env
+  token: process.env.TINA_TOKEN!,
+  client: { skip: true },
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        format: "md",
+        label: "Blog Posts",
+        name: "blog_posts",
+        path: "src/contents",
+        match: {
+          include: "**/*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+          ...blog_postFields(),
+        ],
+      },
+    ],
+  },
+});
+
+export default config;
