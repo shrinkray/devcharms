c85544da5e83e2e159138578b9a36bb23b240c2f
Greg Miller <hello@gregmiller.io>
2026-04-07 16:08:13 -0400

    fix: update frontmatter for rss
    - per dev.to

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,17 +3,24 @@ import rss from "@astrojs/rss";
 import slugify from "@utils/slugify";
 import { allPosts } from "@utils/allPosts";
 
-export const GET = async () =>
-  rss({
+export const GET = async () => {
+  const items = await Promise.all(
+    allPosts
+      .filter(({ frontmatter }) => !frontmatter.draft)
+      .map(async (post) => ({
+        link: `posts/${slugify(post.frontmatter)}`,
+        title: post.frontmatter.title,
+        description: post.frontmatter.description,
+        pubDate: new Date(post.frontmatter.datetime),
+        categories: post.frontmatter.tags?.slice(0, 4) ?? [],
+        content: await post.compiledContent(),
+      })),
+  );
+
+  return rss({
     title: SITE.title,
     description: SITE.desc,
     site: SITE.website,
-    items: allPosts
-      .filter(({ frontmatter }) => !frontmatter.draft)
-      .map(({ frontmatter }) => ({
-        link: `posts/${slugify(frontmatter)}`,
-        title: frontmatter.title,
-        description: frontmatter.description,
-        pubDate: new Date(frontmatter.datetime),
-      })),
+    items,
   });
+};
